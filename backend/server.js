6a328e3c4e504bbca6429ac093304ed988675d90
ankush-khera <ankieekhera@gmail.com>
2024-01-14 15:36:59 +0530

    connect mongoDB to nodeJS

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const {chats} = require("./data");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
-// cross-origin resource sharing
+// cross-origin resource sharing - when frontend and backend are on diff domains
 app.use(cors());
+
+dotenv.config();
+
+connectDB();  // connect to mongoDB database
 
 app.get('/', (req, res)=>{
     res.send("API is running fine!");
