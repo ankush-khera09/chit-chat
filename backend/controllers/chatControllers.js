6a2c78bbaf8d3765e5395c2fbb665b4112076ef7
ankush-khera <ankieekhera@gmail.com>
2024-04-21 18:18:56 +0530

    create new-group-chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -80,6 +80,41 @@ const fetchChats = asyncHandler(
             throw new Error(error.message);
         }
     }
-)
+);
 
-module.exports = { accessChat, fetchChats };
+ const createGroupChat = asyncHandler(
+    // we need users and chat name to create a group chat
+    async(req, res) => {
+        if(!req.body.users || !req.body.name){
+            return res.status(400).send({message: "Please fill all the fields!"});
+        }
+
+        var users = JSON.parse(req.body.users);
+
+        if(users.length < 2){
+            return res.status(400).send("More than 2 users are required to form a group chat!");
+        }
+
+        users.push(req.user);        // to include current logged in user in group chat
+
+        try{
+            const groupChat = await Chat.create({
+                chatName: req.body.name,
+                users: users,
+                isGroupChat: true,
+                groupAdmin: req.user,
+            });
+
+            const fullGroupChat = await Chat.findOne({_id: groupChat._id})
+                .populate("users", "-password")
+                .populate("groupAdmin", "-password");
+                
+                res.status(200).json(fullGroupChat);
+        }catch(error){
+            res.status(400);
+            throw new Error(error.message);
+        }
+    }
+ )
+
+module.exports = { accessChat, fetchChats, createGroupChat };
